ca9ab55997bfa932f7a994b3ab7b7a8e9bd64326
María Paz Muñoz Parra <Mara Paz Muoz Parra>
2016-07-05 19:26:55 -0500

    Reading the json file and writing the info in the html file

--- public/js/index.js
+++ public/js/index.js
@@ -2,31 +2,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /*Json nav */
 var xmlhttp = new XMLHttpRequest();
-var url = "http://www.w3schools.com/website/Customers_MYSQL.php";
+var url = "../data/nav.json";
 
 xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        myFunction(xmlhttp.responseText);
+        navFunction(xmlhttp.responseText);
     }
 }
+
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function myFunction(response) {
-    var arr = JSON.parse(response);
-    var i;
-    var out = "<table>";
+function navFunction(response) {
+	var obj = JSON.parse(response);
+    var out = " ";
 
-    for(i = 0; i < arr.length; i++) {
-        out += "<tr><td>" + 
-        arr[i].Name +
-        "</td><td>" +
-        arr[i].City +
-        "</td><td>" +
-        arr[i].Country +
-        "</td></tr>";
-    }
-    out += "</table>";
+    for (var i in obj) {
+    	var items = obj[i];
+
+    	for (var j in items) {
+	        var items2 = items[j].items;
+
+	        for (var k in items2) {
+	        	out += '<li><a href="' + items2[k].url + '">' + 
+	        	       items2[k].label + '</a></li>';
+	        	
+	        }
+	    }
+	}
+
     document.getElementById("id01").innerHTML = out;
 }
 
